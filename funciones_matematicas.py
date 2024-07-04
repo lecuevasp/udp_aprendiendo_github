51549eda9845dcf3a985b1e7c524bd43ab9e4051
Maximiliano Pino <maximiliano.pino_m@mail.udp.cl>
2024-07-04 18:41:51 -0400

    funcion Potencia

--- funciones_matematicas.py
+++ funciones_matematicas.py
@@ -8,3 +8,13 @@ if __name__ == "__main__":
     b = input('ingrese otro numero ')
     resultado = fn_suma(int(a), int(b))  # Aquí puedes pasar los valores que desees
     print("La suma es:", resultado)
+
+def Potencia (a, b):
+    Potencia1 = a**b
+    Potencia2 = b**a
+    return Potencia1,Potencia2
+
+if __name__ == "__main__":
+    resultado1, resultado2 = Potencia(int(a), int(b)) 
+    print("La potencia a^b es:", resultado1)
+    print("La potencia b^a es:", resultado2)
